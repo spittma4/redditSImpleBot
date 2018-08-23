7c8f71eab7965a917be9c8c370f5de55dac5002d
spittma4 <spittma4@kent.edu>
2018-08-23 00:14:43 -0600

    More work on recognizing keywords

--- recKeyword.py
+++ recKeyword.py
@@ -12,9 +12,32 @@ import os
 reddit = praw.Reddit('cookiemonsterbot')
 subreddit = reddit.subreddit("test")
 
+keyword = "test"
+
 #for submission in reddit.front.hot():
 for submission in subreddit.hot(limit=5):
-	print("Title: ", submission.title)
-	print("Text: ", submission.selftext)
-	print("Score: ", submission.score)
+	title = submission.title
+	text = submission.selftext
+	score = submission.score
+
+	print("Submission ID: " + submission.id) + "\n"
+
+	if (score > 1):
+		print('Score greater than 1!')
+		print('Title: ' + submission.title)
+		print("Score: " + str(submission.score))	
+	else:
+		print("Score less than 2")
+
+	keyword = "test"
+
+	if (keyword in title):
+		print("\nkeyword: 'test' is in title!")
+		print("Title: " + submission.title)
+		keyHighlight = title[:title.find(keyword)] + "***" + keyword + "***" + title[title.find(keyword)+len(keyword):]
+		#print("Keyword Highlight: ", title.find(keyword))
+		print("Keyword Highlight: " + keyHighlight)
+
+	#print("Text: ", submission.selftext)
+	
 	print("-----------------------------------------")
